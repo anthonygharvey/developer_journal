b89ce0b8407dbe4286b5a4a8e926416ceac32c81
anthonygharvey <anthony@anthonygharvey.com>
2018-03-20 00:39:12 -0400

    Add sessions and helper methods to application_controller.  Add presence validation to user model.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,35 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
+#==================== CONFIGURATION =======================
+configure do
+  set :public_folder, 'public'
+  set :views, 'app/views'
+  enable :sessions unless test?
+  set :session_secret, "secret"
+end
+#----------------------------------------------------------
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+
+
+
+
+#==================== HELPERS =============================
+helpers do
+  def current_user
+    User.find(session[:user_id])
   end
 
-  get "/" do
-    erb :welcome
+  def logged_in?
+    !!session[:user_id]
   end
+
+  def valid_signup?
+    !params.any?{|i| i[1].empty?}
+  end
+
+end
+#----------------------------------------------------------
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
 	has_secure_password
 	has_many :goals
 	has_many :entires, through: :goals
-
+	validates_presence_of :username, :email, :password, :first_name, :last_name
 
 end
