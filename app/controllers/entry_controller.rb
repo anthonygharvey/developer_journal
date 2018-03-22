6b73561813e5c85f8a00907a51868de6b5e9aec3
anthonygharvey <anthony@anthonygharvey.com>
2018-03-22 01:27:43 -0400

    Update create_entry view to only show current_user goals.

--- app/controllers/entry_controller.rb
+++ app/controllers/entry_controller.rb
@@ -11,23 +11,17 @@ class EntryController < ApplicationController
 	post '/entries' do
 		@entry = current_user.entries.build(params)
 		if @entry.save
-			# binding.pry
-			flash[:new_entry] = "#{entry.title} saved!"
+			flash[:new_entry] = "#{@entry.title} saved!"
+			# TODO:	Build '/entries/:entry_id'
 			redirect to "/entries/:entry_id"
 		else
 			flash[:entry_error] = @entry.errors.full_messages
-			# binding.pry
 			flash[:unsaved_title] = @entry.title
 			flash[:unsaved_goal] = @entry.goal
 			flash[:unsaved_content] = @entry.content
 			redirect to "/entries/new"
 		end
 	end
-
-
-
-
-		# TODO: uses the valid_entry? helper method.  Confirm the validates_presence_of method that can be added to a model generates an error message.
 	#--------------------------------------------------------
 
 
