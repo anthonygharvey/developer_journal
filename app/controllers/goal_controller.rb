f01fd7bbda9513dafc79f707cb7755f9561c112a
anthonygharvey <anthony@anthonygharvey.com>
2018-03-23 14:04:35 -0400

    Add goals new action and view along with error messages.

--- app/controllers/goal_controller.rb
+++ app/controllers/goal_controller.rb
@@ -1,3 +1,38 @@
 class GoalController < ApplicationController
+	#==================== NEW ===============================
+	get '/goals/new' do
+		if logged_in?
+			erb :'/goals/create_goal'
+		else
+			redirect to "/login"
+			# TODO: Create a flash message asking the user to login first to view the page
+		end
+	end
 
+	post '/goals' do
+		@goal = current_user.goals.build(params[:goal])
+		if @goal.save
+			flash[:new_goal] = "#{@goal.name} was saved!"
+			redirect to "/goals/#{@goal.id}"
+		else
+			flash[:goal_error] = @goal.errors.full_messages
+			flash[:unsaved_name] = @goal.name
+			flash[:unsaved_days] = @goal.duration_in_days
+			flash[:unsaved_languages] = @goal.languages
+			redirect to "/goals/new"
+		end
+	end
+	#--------------------------------------------------------
+
+
+	#==================== SHOW ==============================
+	#--------------------------------------------------------
+
+
+	#==================== EDIT ==============================
+	#--------------------------------------------------------
+
+
+	#==================== DELETE ============================
+	#--------------------------------------------------------
 end
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,7 +1,6 @@
 class Goal < ActiveRecord::Base
 	belongs_to :user
 	has_many :entries
-
-
-
+	validates_presence_of :name, :duration_in_days, :languages
+	validates_uniqueness_of :name
 end
