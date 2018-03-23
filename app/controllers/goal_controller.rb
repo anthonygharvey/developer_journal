796d978e2258358bab8d1db082c5acf560443f9c
anthonygharvey <anthony@anthonygharvey.com>
2018-03-23 19:03:57 -0400

    Add goal show view.

--- app/controllers/goal_controller.rb
+++ app/controllers/goal_controller.rb
@@ -11,6 +11,7 @@ class GoalController < ApplicationController
 
 	post '/goals' do
 		@goal = current_user.goals.build(params[:goal])
+		@goal.start_date = Time.now
 		if @goal.save
 			flash[:new_goal] = "#{@goal.name} was saved!"
 			redirect to "/goals/#{@goal.id}"
