b9b998b07a1c44e2b968d4f3182d8d67777ca0f1
anthonygharvey <anthony@anthonygharvey.com>
2018-03-27 18:07:04 -0400

    undo heroku changes.

--- config/environment.rb
+++ config/environment.rb
@@ -1,16 +1,3 @@
-configure :production do
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-
-  ActiveRecord::Base.establish_connection(
-    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    :host     => db.host,
-    :username => db.user,
-    :password => db.password,
-    :database => db.path[1..-1],
-    :encoding => 'utf8'
-    )
-end
-
 ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
