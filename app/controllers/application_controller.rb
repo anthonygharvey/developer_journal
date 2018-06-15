c2950a9879e154da093ab914fc5b6276f489ce9c
anthonygharvey <anthony@anthonygharvey.com>
2018-06-14 23:31:01 -0400

    Add 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,10 @@ class ApplicationController < Sinatra::Base
     end
   end
   #----------------------------------------------------------
+  not_found do
+    status 404
+    erb :not_found
+  end
 
 
   #==================== HELPERS =============================
