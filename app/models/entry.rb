d17fc0e14d5ebdb7257b756eb78e1e1fb89b9c16
anthonygharvey <anthony@anthonygharvey.com>
2018-06-16 22:18:23 -0400

    Add Sanitize gem. Update goal show view to show table of entries instead of full entries.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -6,4 +6,12 @@ class Entry < ActiveRecord::Base
 		self.created_at.strftime("%A, %B %d, %Y")
 	end
 
+	def snippet
+		if self.content.length > 100
+			Sanitize.fragment(self.content)[0..100]+"..."
+		else
+			self.content
+		end
+	end
+
 end
